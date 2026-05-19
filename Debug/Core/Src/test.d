bdee73b76ee50faf30a1b2124c6399a622ec2720
umastrings <junhaha0315@gmail.com>
2026-05-19 16:21:43 +0900

    BugFIX

--- Debug/Core/Src/test.d
+++ Debug/Core/Src/test.d
@@ -1,4 +1,4 @@
-Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+Core/Src/test.o: ../Core/Src/test.c ../Core/Inc/test.h ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
  ../Core/Inc/stm32f4xx_hal_conf.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
@@ -27,10 +27,10 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
- ../Core/Inc/servo.h ../Core/Inc/main.h ../Core/Inc/encoder.h \
- ../Core/Inc/supersonic.h ../Core/Inc/serial_communication.h \
- ../Core/Inc/encoder.h ../Core/Inc/servo.h ../Core/Inc/supersonic.h \
- ../Core/Inc/test.h
+ ../Core/Inc/servo.h ../Core/Inc/encoder.h ../Core/Inc/supersonic.h \
+ ../Core/Inc/serial_communication.h ../Core/Inc/encoder.h \
+ ../Core/Inc/servo.h ../Core/Inc/supersonic.h
+../Core/Inc/test.h:
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -61,11 +61,9 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
 ../Core/Inc/servo.h:
-../Core/Inc/main.h:
 ../Core/Inc/encoder.h:
 ../Core/Inc/supersonic.h:
 ../Core/Inc/serial_communication.h:
 ../Core/Inc/encoder.h:
 ../Core/Inc/servo.h:
 ../Core/Inc/supersonic.h:
-../Core/Inc/test.h:
